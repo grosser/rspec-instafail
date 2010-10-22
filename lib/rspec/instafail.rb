6f3cb4d2ca9cc2f9a5c5324f5765bc49b4d5002f
grosser <grosser.michael@gmail.com>
2010-10-22 20:13:14 +0200

    make rspec 1.x look like miwillhite made rspec 2.x look

--- lib/rspec/instafail.rb
+++ lib/rspec/instafail.rb
@@ -4,10 +4,25 @@ module RSpec
     require 'spec/runner/formatter/progress_bar_formatter'
     class Instafail < Spec::Runner::Formatter::ProgressBarFormatter
       def example_failed(example, counter, failure)
+        short_padding = '  '
+        padding = '     '
+
         output.puts
-        output.puts red("#{counter}: #{example_group.description} #{example.description}")
-        output.puts " -> #{failure.exception}"
+        output.puts red("#{short_padding}#{counter}) #{example_group.description} #{example.description}")
+        output.puts "#{padding}#{red(failure.exception)}"
+
+        format_backtrace(failure.exception.backtrace).each do |backtrace_info|
+          output.puts insta_gray("#{padding}# #{backtrace_info.strip}")
+        end
+
         output.flush
+      end
+
+      private
+
+      # there is a gray() that returns nil, so we use our own...
+      def insta_gray(text)
+        colour(text, "\e[90m")
       end
     end
     Instafail
@@ -19,16 +34,20 @@ module RSpec
       def example_failed(example)
         @counter ||= 0
         @counter += 1
+
         exception = example.metadata[:execution_result][:exception_encountered]
         short_padding = '  '
         padding = '     '
+
         output.puts
         output.puts "#{short_padding}#{@counter}) #{example.full_description}"
         output.puts "#{padding}#{red("Failure/Error:")} #{red(read_failed_line(exception, example).strip)}"
         output.puts "#{padding}#{red(exception)}"
+
         format_backtrace(exception.backtrace, example).each do |backtrace_info|
           output.puts grey("#{padding}# #{backtrace_info}")
         end
+
         output.flush
       end
     end
