c3ad73016f2cd58569eb98a68475f0ab42813c6d
Matt Willhite <matthew.willhite@gmail.com>
2010-10-25 21:36:00 -0700

    Adapted specs for new output, also attempted to make them a little more legible

--- spec/instafail_spec.rb
+++ spec/instafail_spec.rb
@@ -1,13 +1,54 @@
 describe 'RSpec::Instafail' do
   it "works correctly with RSpec 1.x" do
     output = `cd spec/rspec_1 && bundle exec spec a_test.rb --format RSpec::Instafail`
-    output.should include("1: x a\n -> expected: 2,\n     got: 1 (using ==)\n\.\.\*\.")
-    output.should =~ /Finished in \d\.\d+ seconds/
+    expected_output = <<EXP
+  1\\) x a
+     expected: 2,
+     got: 1 \\(using ==\\)
+     # \\.\\/a_test\\.rb:5:
+\\.\\.\\*\\.
+
+Pending:
+
+x d \\(TODO\\)
+\\.\\/a_test\\.rb:14
+
+1\\)
+'x a' FAILED
+expected: 2,
+     got: 1 \\(using ==\\)
+\\./a_test\\.rb:5:
+
+Finished in \\d\\.\\d+ seconds
+
+5 examples, 1 failure, 1 pending
+EXP
+
+    output.should =~ Regexp.new(expected_output, 'x')
+
   end
 
   it "works correctly with RSpec 2.x" do
     output = `cd spec/rspec_2 && bundle exec rspec a_test.rb --require ../../lib/rspec/instafail --format RSpec::Instafail --no-color`
-    output.should =~ /1: x a\n -> expected: 2,\n     got: 1 .*\.\.\*\./m
-    output.should =~ /Finished in \d\.\d+ seconds/
+    expected_output = <<EXP
+  1\\) x a
+     Failure\\/Error: 1\\.should == 2
+     expected: 2,
+     got: 1 \\(using ==\\)
+     # \\./a_test\\.rb:5
+\\.\\.\\*\\.
+
+Pending:
+  x d
+    # No reason given
+    # \\./a_test\\.rb:14
+
+Finished in \\d\\.\\d+ seconds
+5 examples, 1 failure, 1 pending
+EXP
+
+    output.should =~ Regexp.new(expected_output, 'x')
+    
   end
 end
+
