8a9308e54ac274e472a80325712cdf38ef93038e
Steve Tooke <steve.tooke@gmail.com>
2010-11-16 22:45:05 +0000

    Extract rspec 1 & 2 versions to separate files

--- lib/rspec/instafail.rb
+++ lib/rspec/instafail.rb
@@ -2,49 +2,11 @@ module RSpec
   begin
     # rspec 2.x
     require 'rspec/core/formatters/progress_formatter'
-    class Instafail < RSpec::Core::Formatters::ProgressFormatter
-      def example_failed(example)
-        @counter ||= 0
-        @counter += 1
-        exception = example.metadata[:execution_result][:exception_encountered]
-        short_padding = '  '
-        padding = '     '
-        output.puts
-        output.puts "#{short_padding}#{@counter}) #{example.full_description}"
-        output.puts "#{padding}#{red("Failure/Error:")} #{red(read_failed_line(exception, example).strip)}"
-        output.puts "#{padding}#{red(exception)}"
-        format_backtrace(exception.backtrace, example).each do |backtrace_info|
-          output.puts grey("#{padding}# #{backtrace_info}")
-        end
-        output.flush
-      end
-    end
+    require 'rspec/instafail/rspec_2'
   rescue LoadError => try_rspec_1
     # rspec 1.x
     require 'spec/runner/formatter/progress_bar_formatter'
-    class Instafail < Spec::Runner::Formatter::ProgressBarFormatter
-      def example_failed(example, counter, failure)
-        short_padding = '  '
-        padding = '     '
-
-        output.puts
-        output.puts red("#{short_padding}#{counter}) #{example_group.description} #{example.description}")
-        output.puts "#{padding}#{red(failure.exception)}"
-
-        [*format_backtrace(failure.exception.backtrace)].each do |backtrace_info|
-          output.puts insta_gray("#{padding}# #{backtrace_info.strip}")
-        end
-
-        output.flush
-      end
-
-      private
-
-      # there is a gray() that returns nil, so we use our own...
-      def insta_gray(text)
-        colour(text, "\e[90m")
-      end
-    end
+    require 'rspec/instafail/rspec_1'
   end
 
   Instafail::VERSION = File.read( File.join(File.dirname(__FILE__),'..','..','VERSION') ).strip
