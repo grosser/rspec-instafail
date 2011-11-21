3a80c0734e7ec8162d5dfe435c9a12740a5df2d6
grosser <grosser.michael@gmail.com>
2011-11-21 08:30:22 -0800

    modify load-path hack

--- lib/rspec/instafail.rb
+++ lib/rspec/instafail.rb
@@ -1,12 +1,14 @@
 module RSpec
-  # when installed as plugin in jruby, lib is not in load-path
+  # Rails 3: when installed as plugin, lib is not in load-path when running tests
   # https://github.com/grosser/rspec-instafail/pull/3
-  lib = File.dirname(File.dirname(__FILE__))
-  begin
-    require "#{lib}/rspec/instafail/rspec_2"
-  rescue LoadError # try rspec 1
-    require "#{lib}/rspec/instafail/rspec_1"
-  end
+  lib = File.expand_path(File.dirname(File.dirname(__FILE__)))
+  $LOAD_PATH << lib unless $LOAD_PATH.include?(lib)
 
   require 'rspec/instafail/version'
+
+  begin
+    require "rspec/instafail/rspec_2"
+  rescue LoadError # try rspec 1
+    require "rspec/instafail/rspec_1"
+  end
 end
